0c6e05c48db8c032f8e7fd2d875510eac98048bd
therufuser <therufuser@gmail.com>
2020-12-28 02:19:45 +0100

    feat(volcano): Implement buffer creation

--- src/volcano/renderer.hpp
+++ src/volcano/renderer.hpp
@@ -6,6 +6,11 @@
 #define MAX_SYNC 3
 
 namespace volcano {
+	struct buffer {
+		VkBuffer buffer;
+		VkDeviceMemory memory;
+	};
+
 	class renderer {
 		private:
 			VkPhysicalDeviceProperties gpu_properties;
@@ -15,6 +20,12 @@ namespace volcano {
 			uint32_t swapchain_mask;
 
 			VkPipelineCache pipeline_cache;
+
+			struct buffer ubo[MAX_SYNC];
+
+			uint32_t find_memory_type_from_requirements(uint32_t device_requirements, uint32_t host_requirements);
+			struct buffer create_buffer(const void *initial, size_t size, VkBufferUsageFlags usage);
+			void init_uniform_buffer();
 
 		public:
 			void init(retro_hw_render_interface_vulkan *);
