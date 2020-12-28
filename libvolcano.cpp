b15143543fb402c53a0e22ec6a5c224cec56afa6
therufuser <therufuser@gmail.com>
2020-12-28 16:26:24 +0100

    refactor(libvolcano): Move stuff around for better structure

--- libvolcano.cpp
+++ libvolcano.cpp
@@ -4,7 +4,10 @@
 
 const int w = 1280;
 const int h = 720;
-static unsigned short buffer[w * h];
+
+static const char* library_name = "Archaic Volcano";
+static const char* library_version = "v0.0.1";
+static const char* valid_extensions = "";
 
 static struct retro_callbacks {
   retro_video_refresh_t video;
@@ -14,8 +17,22 @@ static struct retro_callbacks {
   retro_input_state_t input_state;
 } retro_callbacks;
 
+static unsigned short buffer[w * h];
+
+// Forward declarations
+unsigned int abs(int);
+
+// Core basics
 RETRO_API unsigned int retro_api_version () {
   return RETRO_API_VERSION;
+}
+
+RETRO_API void retro_get_system_info(retro_system_info* info) {
+  info->library_name     = library_name;
+  info->library_version  = library_version;
+  info->valid_extensions = valid_extensions;
+  info->need_fullpath    = false;
+  info->block_extract    = false;
 }
 
 RETRO_API void retro_set_environment(retro_environment_t cb) {
@@ -28,6 +45,8 @@ RETRO_API void retro_init() {
   loguru::g_stderr_verbosity = 1;
   LOG_F(INFO, "Logger initialized!");
 }
+
+RETRO_API void retro_deinit() {}
 
 RETRO_API void retro_set_video_refresh(retro_video_refresh_t cb) {
   retro_callbacks.video = cb;
@@ -54,18 +73,7 @@ RETRO_API void retro_set_controller_port_device(
 	[[maybe_unused]] unsigned device
 ) {}
 
-static const char* library_name = "Archaic Volcano";
-static const char* library_version = "v0.0.1";
-static const char* valid_extensions = "";
-
-RETRO_API void retro_get_system_info(retro_system_info* info) {
-  info->library_name     = library_name;
-  info->library_version  = library_version;
-  info->valid_extensions = valid_extensions;
-  info->need_fullpath    = false;
-  info->block_extract    = false;
-}
-
+// Core runtime stuff
 RETRO_API bool retro_load_game([[maybe_unused]] const struct retro_game_info* game) {
   return true;
 }
@@ -91,31 +99,8 @@ RETRO_API void retro_get_system_av_info(retro_system_av_info *info) {
   };
 }
 
-RETRO_API size_t retro_serialize_size(void) {
-  return 0;
-}
-
-RETRO_API void retro_reset(void) {}
-
-RETRO_API void retro_unload_game(void) {}
-
 RETRO_API unsigned retro_get_region(void) {
   return RETRO_REGION_PAL;
-}
-
-RETRO_API void* retro_get_memory_data([[maybe_unused]] unsigned id) {
-  return nullptr;
-}
-
-RETRO_API size_t retro_get_memory_size([[maybe_unused]] unsigned id) {
-  return 0;
-}
-
-unsigned int abs(int x) {
-  if(x < 0)
-    return -x;
-
-  return x;
 }
 
 RETRO_API void retro_run(void) {
@@ -129,6 +114,24 @@ RETRO_API void retro_run(void) {
   }
 
   retro_callbacks.video(buffer, w, h, sizeof(unsigned short) * w);
+}
+
+RETRO_API void retro_reset(void) {}
+
+RETRO_API void retro_unload_game(void) {}
+
+// Memory extraction
+RETRO_API size_t retro_get_memory_size([[maybe_unused]] unsigned id) {
+  return 0;
+}
+
+RETRO_API void* retro_get_memory_data([[maybe_unused]] unsigned id) {
+  return nullptr;
+}
+
+// Serialization
+RETRO_API size_t retro_serialize_size(void) {
+  return 0;
 }
 
 RETRO_API bool retro_serialize(
@@ -145,6 +148,7 @@ RETRO_API bool retro_unserialize(
   return false;
 }
 
+// Cheat related
 RETRO_API void retro_cheat_reset(void) {}
 
 RETRO_API void retro_cheat_set(
@@ -153,4 +157,10 @@ RETRO_API void retro_cheat_set(
   [[maybe_unused]] const char *code
 ) {}
 
-RETRO_API void retro_deinit() {}
+// Misc.
+unsigned int abs(int x) {
+  if(x < 0)
+    return -x;
+
+  return x;
+}
