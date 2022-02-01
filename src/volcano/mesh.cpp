8c442aa155bdcd60a0dc0d2b712038766c9397ff
therufuser <therufuser@gmail.com>
2022-02-01 23:40:04 +0100

    feat(mesh): Allow storing descriptorsets and uniform-buffers with meshes
    This adds private members that store descriptor sets and uniform-buffer
    references.
    
    Also implements getters and setters for these attributes.

--- src/volcano/mesh.cpp
+++ src/volcano/mesh.cpp
@@ -4,12 +4,23 @@
 
 namespace volcano {
 	mesh::mesh(
+		const std::array<VkDescriptorSet, MAX_SYNC> descriptor_sets,
+		const std::array<struct buffer, MAX_SYNC> uniform_buffers,
 		const struct buffer vbo,
 		const int size,
 		const kepler::transform_reference& transform
 	)
-	: vbo(vbo), vert_count(size), transforms({transform, transform, transform, transform}) {
+	: descriptor_sets(descriptor_sets), uniform_buffers(uniform_buffers),
+	  vbo(vbo), vert_count(size), transforms({transform, transform, transform, transform}) {
 		LOG_F(MAX, "Create new mesh (size=%d)", size);
+	}
+
+	const VkDescriptorSet& mesh::get_descriptor_set(const int index) {
+		return descriptor_sets[index];
+	}
+
+	const struct buffer& mesh::get_uniform(const int index) {
+		return uniform_buffers[index];
 	}
 
 	VkBuffer_T *const *mesh::get_buffer() {
--- src/volcano/mesh.hpp
+++ src/volcano/mesh.hpp
@@ -9,6 +9,9 @@
 namespace volcano {
 	class mesh {
 		private:
+			std::array<VkDescriptorSet, MAX_SYNC> descriptor_sets;
+			std::array<struct buffer, MAX_SYNC> uniform_buffers;
+
 			struct buffer vbo;
 			int vert_count;
 
@@ -16,11 +19,15 @@ namespace volcano {
 
 		public:
 			mesh(
+				const std::array<VkDescriptorSet, MAX_SYNC> descriptor_sets,
+				const std::array<struct buffer, MAX_SYNC> uniform_buffers,
 				const struct buffer vbo,
 				const int size,
 				const kepler::transform_reference& transform
 			);
 
+			const VkDescriptorSet& get_descriptor_set(const int index);
+			const struct buffer& get_uniform(const int index);
 			VkBuffer_T *const *get_buffer();
 			const int& get_size();
 			kepler::transform_reference& get_transform(const int index);
